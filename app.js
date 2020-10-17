212be9843cfbdbc9c8935899ac509d3759678a41
AIS0001 <axialitsolutions0001@gmail.com>
2020-10-17 22:08:56 +0700

    appjs change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000; 
 const userRouter = require("./api/users/user.router");
 app.use(express.json());
 app.use("/api/users",userRouter);
