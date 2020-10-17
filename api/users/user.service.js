13b317755df425d1e2b3cda380bbd2ed5e6c8100
AIS0001 <axialitsolutions0001@gmail.com>
2020-10-17 21:30:46 +0700

    reday to go

---
+++ api/users/user.service.js
@@ -0,0 +1,101 @@
+const pool = require("../../config/database");
+
+module.exports = {
+    create:(data,callback)=>{
+        pool.query(
+            `INSERT INTO registration ( fname, lname, gender, email, password, contact)
+             VALUES ( ?,?,?,?,?,?)`,
+        [
+            data.fname,
+            data.lname,
+            data.gender,
+            data.email,
+            data.password,
+            data.contact
+        ],
+        (error,results,fields)=>{
+            if(error)
+            {
+                return callback(error);
+            }
+            return callback(null,results);
+        }
+
+        );
+    },
+
+    getUsers:callback=>{
+        pool.query(`select id ,fname,lname,gender,email,contact from registration`,
+        [
+
+        ],
+        (error,results,fields)=>{
+            if(error)
+            {
+              return  callback(error);
+            }
+            return callback(null,results);
+        }
+        );
+    },
+
+    getUserByid:(id,callback)=>{
+        pool.query(`select fname,lname,gender,email,contact from registration where id=?`,
+        [id],
+        (error,results,fields)=>{
+            if(error)
+            {
+              return  callback(error);
+            }
+            return callback(null,results);
+        }
+        );
+    },
+
+    updateUser:(data,callback)=>{
+        pool.query(`update registration set fname=?,lname-?,gender=?,email=?,password=?,contact=? where id=?`,
+        [
+            data.fname,
+            data.lname,
+            data.gender,
+            data.email,
+            data.password,
+            data.contact,
+            data.id
+        ],
+        (error,results,fields)=>{
+            if(error)
+            {
+              return  callack(error);
+            }
+            return callback(null,results);
+        }
+        );
+    },
+    deleteUser:(data,callback)=>{
+        pool.query(`delete from registration where id=?`,
+        [data.id],
+        (error,results,fields)=>{
+            if(error)
+            {
+              return  callback(error);
+            }
+            return callback(null,results);
+        }
+        );
+    },
+    getUserByuserEmail:(email,callack)=>{
+        pool.query(
+            `select * from registration where email = ?`,
+            [email],
+            (error,results,fields)=>{
+                if(error)
+                {
+                    callack(error);
+                }
+                return callack(null,results[0]);
+            }
+        );
+    }
+
+};
---
+++ auth/token_validation.js
@@ -0,0 +1,30 @@
+const { verify } = require("jsonwebtoken");
+
+module.exports = {
+    checkToken:(req,res,next)=>{
+        let token = req.get("autorization");
+        if(token)
+        {
+            token = token.slice(7);
+            verify(token,"MyLovePriyanka",(err,decoded)=>{
+                if(err)
+                {
+                    res.json({
+                        success:0,
+                        message:"Invalid token"
+
+                    })
+                }else{
+                    next();
+                }
+            })
+        }
+        else{
+            res.json({
+                success:0,
+                message:"Access Denied"
+            })
+        }
+    }
+
+}
